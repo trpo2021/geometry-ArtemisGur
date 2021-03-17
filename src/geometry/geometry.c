e9a6ef2db376971938addfe81e9ed58cf0c5c1e5
Artem <love-gura@yandex.ru>
2021-03-17 17:07:41 +0700

    Added the ability to enter multiple shapes

--- src/geometry/geometry.c
+++ src/geometry/geometry.c
@@ -4,28 +4,36 @@
 
 int main(void)
 {
-    CircleTokens circle_struct;
+    int number_of_figures = 0;
     char input_data[MAX_SIZE_OF_STRING];
-    printf("Input data: \n");
-    fgets(input_data, MAX_SIZE_OF_STRING, stdin);
+    int length = 1;
+    printf("Enter the data or \"0\" to exit the app: \n");
+    // fgets(input_data, MAX_SIZE_OF_STRING, stdin);
     bool correct_check = true;
-
+    CircleTokens* array_of_shapes = malloc(sizeof(CircleTokens));
     char example_of_circle[] = "circle";
-    int data_length = strlen(input_data);
-
-    circle_struct = make_circle_tokens(data_length, input_data, circle_struct);
-
-    char figure[circle_struct.op_bracket];
-    strncpy(figure, input_data, circle_struct.op_bracket);
-
-    if (strcmp(figure, example_of_circle) != 0)
+    char figure[7];
+    while (*fgets(input_data, MAX_SIZE_OF_STRING, stdin) != '0')
     {
-        printf("Incorrect figure. Expected \"circle\" or \"triangle\".\n.");
-        return -1;
+        int data_length = strlen(input_data);
+        array_of_shapes = realloc(array_of_shapes, sizeof(CircleTokens) * length);
+        CircleTokens circle_struct;
+        circle_struct = make_circle_tokens(data_length, input_data, circle_struct);
+        strncpy(figure, input_data, circle_struct.op_bracket);
+
+        if (strcmp(figure, example_of_circle) != 0)
+        {
+            printf("Incorrect figure. Expected \"circle\" or \"triangle\".\n.");
+            return -1;
+        }
+
+        correct_check = fill_circle_coord(&circle_struct, input_data);
+        array_of_shapes[number_of_figures] = circle_struct;
+
+        print_figure(correct_check, figure, circle_struct, find_circle_perimeter(circle_struct),
+                     find_circle_area(circle_struct));
+        number_of_figures++;
+        length++;
+        printf("\n");
     }
-
-    correct_check = fill_circle_coord(&circle_struct, input_data);
-
-    print_figure(correct_check, figure, circle_struct, find_circle_perimeter(circle_struct),
-                 find_circle_area(circle_struct));
 }
